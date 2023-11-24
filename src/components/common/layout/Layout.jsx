9c9698afe2918044999fdfa227901fb8786eaf59
eunhye <masimel33@gmail.com>
2023-11-24 12:26:24 +0900

    layout 컴포넌트에 모션 추가 - useRef,useEffect

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,17 +1,25 @@
+import { useEffect, useRef } from 'react';
 import './Layout.scss';
 
 export default function Layout({ children, title }) {
+	const refFrame = useRef(null);
+
+	useEffect(() => {
+		refFrame.current.classList.add('on');
+	}, []);
+
 	return (
-		<main className={`Layout ${title}`}>
+		<main ref={refFrame} className={`Layout ${title}`}>
 			<h1>{title}</h1>
 			<div className='bar'></div>
-			{/*Layout 컴포넌트로 감싼 컨텐츠 내용이 아래 children위치에 출력됨  */}
+
 			{children}
 		</main>
 	);
 }
 
 /*
+Layout 컴포넌트로 감싼 컨텐츠 내용이 아래 children위치에 출력됨   
 부모태그 안쪽 컨텐츠가 children으로 받아짐 
 
 */

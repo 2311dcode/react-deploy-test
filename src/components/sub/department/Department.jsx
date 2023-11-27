abd8e7b321ea555964bf47b2c04a7c4211f9b482
phoebe7010 <phoebe7010@daum.net>
2023-11-27 13:17:21 +0900

    문자열메소드 이용하여 h2텍스트

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -3,6 +3,11 @@ import Layout from '../../common/layout/Layout';
 import './Department.scss';
 
 export default function Department() {
+	const test = 'abcde';
+	console.log(test.charAt(0)); //a
+	console.log(test.slice(1, 3)); //bc
+	console.log(test.slice(1)); //bcde
+	console.log(test.toUpperCase());
 	const [MemberTit, setMemberTit] = useState('');
 
 	const [MemberData, setMemberData] = useState([]);
@@ -30,7 +35,7 @@ export default function Department() {
 	return (
 		<Layout title={'Department'}>
 			<section className='memberBox'>
-				<h2>{MemberTit}</h2>
+				<h2>{`${MemberTit.charAt(0).toUpperCase() + MemberTit.slice(1)}`}</h2>
 				{MemberData.map((member, idx) => {
 					return (
 						<article key={member + idx}>
@@ -84,5 +89,15 @@ const student ={name:'David', age:20}
 Object.keys(student);['name','age']
 Object.values(student); ['David',20]			
 
+
+문자열 관련 내장 메서드 
 특정문자에서 첫 단어만 대문자로 변경 
+전체문자열.charAt(순서) : 전체 문자열에서 해당 순서의 문자값만 반환 
+전체문자열.slice(순서1,순서2) : 전체 문자열에서 해당순서1부터 순서2(없다면 끝까지) 문자를 잘라서 반환 
+전체문자열.upperCase() : 문자열을 전체 대문자로 반환 
+전체문자열.lowerCase() :문자열을 전체 소문자로 반환 
+전체문자열.split(구분자) : 전체문자열을 구분자를 기준으로 나눠서 배열로 반환 
+배열.join('구분자') : 각 배열값을 구분자로 이어붙이면서 하나의 문자열로 반환 
+
+//
 */

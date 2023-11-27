7a7029b5407787f620169af2aad4dcae21733629
phoebe7010 <phoebe7010@daum.net>
2023-11-27 11:35:43 +0900

    다크테마 적용

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
 	return (
 		<div className={Dark ? 'wrap dark' : 'wrap'}>
-			<Header />
+			<Header Dark={Dark} setDark={setDark} />
 			<Route exact path='/' component={MainWrap} />
 			<Route path='/department' component={Department} />
 			<Route path='/youtube' component={Youtube} />

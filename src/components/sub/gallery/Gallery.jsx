3323c7914ed8f237b27829696340c5c72d9ee789
phoebe7010 <phoebe7010@daum.net>
2023-12-01 11:11:31 +0900

    flickr gallery 세팅

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,6 +1,41 @@
+import { useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 
 export default function Gallery() {
-	return <Layout title={'Gallery'}>Gallery</Layout>;
+	const [Pics, setPics] = useState([]);
+
+	const fetchFlickr = async () => {
+		const num = 500;
+		const flickr_api = '78939f1ef402bce6ea05861acf0b4676';
+		const method_interest = 'flickr.interestingness.getList';
+		const baseURL = 'https://www.flickr.com/services/rest/?method=';
+		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
+
+		const data = await fetch(resultURL);
+		const json = await data.json();
+		console.log(json.photos.photo);
+
+		setPics(json.photos.photo);
+	};
+
+	useEffect(() => {
+		fetchFlickr();
+	}, []);
+	return (
+		<Layout title={'Gallery'}>
+			{Pics.map((pic, idx) => {
+				return (
+					<article key={pic.id}>
+						<h2>{pic.id}</h2>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
+
+/* 
+https://www.flickr.com/services/rest/?method=flickr.test.echo&name=value
+flickr.interestingness.getList
+*/

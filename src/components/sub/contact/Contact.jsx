ee769101055688e27a93fce350fc85e7598a7646
phoebe7010 <phoebe7010@daum.net>
2023-12-11 12:44:35 +0900

    kakaomap roadview

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -7,8 +7,15 @@ export default function Contact() {
 
 	//화면에 출력될 지도정보 배열의 순번이 담길 state
 	const [Index, setIndex] = useState(0);
+	const [Traffic, setTraffic] = useState(false);
+	const [View, setView] = useState(false);
+
 	const mapFrame = useRef(null);
+	const viewFrame = useRef(null);
+
 	const marker = useRef(null);
+	const mapInstance = useRef(null);
+
 	//지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
 	const mapInfo = useRef([
 		{
@@ -40,25 +47,76 @@ export default function Contact() {
 		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt),
 	});
 
+	const roadview = () => {
+		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 50, (panoId) => {
+			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
+		});
+	};
+
+	const setCenter = () => {
+		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
+		roadview();
+	};
+
 	//컴포넌트 마운트시 참조객체에 담아놓은 돔 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
-		const mapInstance = new kakao.current.maps.Map(mapFrame.current, {
+		mapFrame.current.innerHTML = '';
+		mapInstance.current = new kakao.current.maps.Map(mapFrame.current, {
 			center: mapInfo.current[Index].latlng,
 			level: 3,
 		});
-		marker.current.setMap(mapInstance);
+		marker.current.setMap(mapInstance.current);
+		setTraffic(false);
+		setView(false);
+
+		roadview();
+
+		//지도 타입 컨트롤러 추가
+		mapInstance.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
+
+		//지도 줌 컨트롤러 추가
+		mapInstance.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
+
+		//휠에 맵 줌 기능 비활성화
+		mapInstance.current.setZoomable(false);
+
+		window.addEventListener('resize', setCenter);
+		return () => window.removeEventListener('resize', setCenter);
 	}, [Index]);
+
+	useEffect(() => {
+		Traffic
+			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
+			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
+	}, [Traffic]);
 
 	return (
 		<Layout title={'Contact'}>
-			<ul className='branch'>
-				{mapInfo.current.map((el, idx) => (
-					<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
-						{el.title}
-					</button>
-				))}
-			</ul>
-			<article className='mapBox' ref={mapFrame}></article>
+			<div className='controlBox'>
+				<nav className='branch'>
+					{mapInfo.current.map((el, idx) => (
+						<button key={idx} onClick={() => setIndex(idx)} className={idx === Index ? 'on' : ''}>
+							{el.title}
+						</button>
+					))}
+				</nav>
+
+				<nav className='info'>
+					<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
+					<button onClick={() => setView(!View)}>{View ? 'map' : 'road view'}</button>
+					<button onClick={setCenter}>위치 초기화</button>
+				</nav>
+			</div>
+			<section className='tab'>
+				<article className={`mapBox ${View ? '' : 'on'}`} ref={mapFrame}></article>
+				<article className={`viewBox ${View ? 'on' : ''}`} ref={viewFrame}></article>
+			</section>
 		</Layout>
 	);
 }
+
+/*
+	1.cdn불러온 window에 불러온 외부 객체값을 가져와서 인스턴스 생성
+	2.인스턴스값을 참조객체 담는 이유 (의존성배열에 불필요하게 등록하지 않기 위해서)
+	3.화면변경점이 발생해야 될떄 state값에 따라서 변경되게 로직화한 다음 이벤트 발생시 state를 변경해서 화면 재랜더링 유도
+*/

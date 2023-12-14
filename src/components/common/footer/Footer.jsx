88d544997e9ea8d989742df8b5175ee86c651992
phoebe7010 <phoebe7010@daum.net>
2023-12-14 12:34:01 +0900

    footer에서 전역데이터 호출

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,14 +1,18 @@
 import './Footer.scss';
 import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { UseSelector, useSelector } from 'react-redux';
 //npm i react-icons
 
 export default function Footer() {
+	const { name, position } = useSelector(store => store.memberReducer.members[0]);
 	return (
 		<footer className='Footer'>
 			<h1>Dcodelab</h1>
 
 			<p>2023 Dcodelab &copy; All Rights Reserved.</p>
-
+			<p>
+				{position}:{name}
+			</p>
 			<ul>
 				<li>
 					<FaFacebookF />

60d27181c73f499522f2cb2f8736cbb7f5ca39dd
phoebe7010 <phoebe7010@daum.net>
2023-12-22 12:00:04 +0900

    community 수정

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -9,7 +9,7 @@ export default function Community() {
 	const changeText = useCustomText('combined');
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
-		return JSON.parse(data);
+		if (data) return JSON.parse(data);
 	};
 	const [Post, setPost] = useState(getLocalData());
 	const refTit = useRef(null);

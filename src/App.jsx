8f7bd7bc0f6c0d253e7d8ad5fb1eb71e158c0a2b
phoebe7010 <phoebe7010@daum.net>
2023-12-27 13:06:40 +0900

    react-query로 history, department 데이터 관리

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import './globalStyles/Variables.scss';
 import './globalStyles/Reset.scss';
 import { useState } from 'react';
 import { useMedia } from './hooks/useMedia';
-import Menu from './components/common/memu/Menu';
+import Menu from './components/common/menu/Menu';
 import Detail from './components/sub/youtube/Detail';
 import Welcome from './components/sub/members/Welcome';
 
--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -2,32 +2,34 @@ import { useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Department.scss';
 import { useCustomText } from '../../../hooks/useText';
-import { useSelector } from 'react-redux';
+import { useHistoryQuery } from '../../../hooks/useHistoryQuery';
+import { useDepartmentQuery } from '../../../hooks/useDepartmentQuery';
 
 export default function Department() {
-	const MemberData = useSelector(store => store.memberReducer.members);
-	const HistoryData = useSelector(store => store.historyReducer.history);
-	console.log(HistoryData);
-	const combinedTitle = useCustomText('combined');
 	const path = useRef(process.env.PUBLIC_URL);
+	const combinedTitle = useCustomText('combined');
+
+	const { data: HistoryData, isSuccess: isHistory } = useHistoryQuery();
+	const { data: MemberData, isSuccess: isMember } = useDepartmentQuery();
 
 	return (
 		<Layout title={'Department'}>
 			<section className='historyBox'>
 				<h2>{combinedTitle('History')}</h2>
 				<div className='con'>
-					{HistoryData?.map((history, idx) => {
-						return (
-							<article key={history + idx}>
-								<h3>{Object.keys(history)[0]}</h3>
-								<ul>
-									{Object.values(history)[0].map((list, idx) => {
-										return <li key={list + idx}>{list}</li>;
-									})}
-								</ul>
-							</article>
-						);
-					})}
+					{isHistory &&
+						HistoryData.map((history, idx) => {
+							return (
+								<article key={history + idx}>
+									<h3>{Object.keys(history)[0]}</h3>
+									<ul>
+										{Object.values(history)[0].map((list, idx) => {
+											return <li key={list + idx}>{list}</li>;
+										})}
+									</ul>
+								</article>
+							);
+						})}
 				</div>
 			</section>
 
@@ -35,17 +37,18 @@ export default function Department() {
 				<h2>{combinedTitle('Members')}</h2>
 
 				<div className='con'>
-					{MemberData?.map((member, idx) => {
-						return (
-							<article key={member + idx}>
-								<div className='pic'>
-									<img src={`${path.current}/img/${member.pic}`} alt={member.name} />
-								</div>
-								<h3>{member.name}</h3>
-								<p>{member.position}</p>
-							</article>
-						);
-					})}
+					{isMember &&
+						MemberData.map((member, idx) => {
+							return (
+								<article key={member + idx}>
+									<div className='pic'>
+										<img src={`${path.current}/img/${member.pic}`} alt={member.name} />
+									</div>
+									<h3>{member.name}</h3>
+									<p>{member.position}</p>
+								</article>
+							);
+						})}
 				</div>
 			</section>
 		</Layout>

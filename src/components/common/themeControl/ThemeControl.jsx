c598fdb42f3cdc3e993b6b400f1f286d539bf081
phoebe7010 <phoebe7010@daum.net>
2023-12-28 15:40:15 +0900

    darkMode 컴포넌트 분리

--- src/components/common/themeControl/ThemeControl.jsx
+++ src/components/common/themeControl/ThemeControl.jsx
@@ -26,7 +26,7 @@ export default function ThemeControl() {
 				type='color'
 				ref={inputEl}
 				onChange={changeThemeColor}
-				defaultValue={isCookie('theme') ? document.cookie.split('theme=')[1].split(';')[0] : 'hotpink'}
+				defaultValue={isCookie('theme') ? document.cookie.split('theme=')[1].split(';')[0] : '#ff69b4'}
 			/>
 			{/* <button onClick={changeThemeColor}>theme color</button> */}
 		</nav>
@@ -38,5 +38,9 @@ export default function ThemeControl() {
 1.클릭이벤트로 컬러팔레트에서 선택한 색상 코드 값을 쿠키로 저장 
 2. App 마운트시 --pointColor에 등록된 value값을 쿠키에 있는 값으로 변경 처리 
 
+
+//skku1006
+//미션1 - 테마초기화버튼 생성한뒤 해당 버튼 클릭시 css변수에 등록되어 있는 색상값으로 초기화
+//미션2 - Dark모드의 값도 쿠키에 등록을 해서 한번 설정된 값으로 유지되도록 처리
 
 */

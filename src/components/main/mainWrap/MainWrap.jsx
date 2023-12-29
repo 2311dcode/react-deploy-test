31018a79426c8a14d5e6f353624a142fe32dce00
phoebe7010 <phoebe7010@daum.net>
2023-12-29 15:13:31 +0900

    button hover 모션, 딜레이설정

--- src/components/main/mainWrap/MainWrap.jsx
+++ src/components/main/mainWrap/MainWrap.jsx
@@ -1,11 +1,14 @@
 import Info from '../info/Info';
+import Pics from '../pics/Pics';
 import Visual from '../visual/Visual';
+
 import './MainWrap.scss';
 
 export default function MainWrap() {
 	return (
 		<div className='MainWrap'>
 			<Visual />
+			<Pics />
 			<Info />
 		</div>
 	);
--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,24 +1,21 @@
 import './Visual.scss';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Link } from 'react-router-dom';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
 import { useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
-import { Autoplay, Pagination, Navigation } from 'swiper';
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-// import 'swiper/css/scrollbar';
-import { Link } from 'react-router-dom';
 import { useCustomText } from '../../../hooks/useText';
 
 function Btns() {
+	//Swiper컴포넌트 안쪽에 있는 또다른 자식 컴포넌트 안쪽에서만 useSwiper hook사용가능
+	//hook으로부터 생성된 객체(인스턴스)에는 다양한 prototype메서드와 property값 활용가능
 	const swiper = useSwiper();
 
 	useEffect(() => {
 		swiper.init(0);
 		swiper.slideNext(300);
-		//화면로딩시 첫 슬라이드 보이게
 	}, [swiper]);
 
 	return (
@@ -37,15 +34,11 @@ function Btns() {
 
 export default function Visual() {
 	const { youtube } = useSelector(store => store.youtubeReducer);
-	// console.log(youtube);
 	const shortenText = useCustomText('shorten');
+
 	return (
 		<figure className='Visual'>
 			<Swiper
-				initialSlide='0'
-				centeredSlides={true}
-				// runCallbacksOnInit={true}
-				slidesPerView={1}
 				modules={[Pagination, Autoplay]}
 				pagination={{
 					clickable: true,
@@ -54,8 +47,7 @@ export default function Visual() {
 					}
 				}}
 				autoplay={{
-					delay: 2000,
-					//	pauseOnMouseEnter: true,
+					delay: 4000,
 					disableOnInteraction: true
 				}}
 				loop={true}>
@@ -74,17 +66,26 @@ export default function Visual() {
 								</div>
 								<div className='txtBox'>
 									<h2>{shortenText(vid.snippet.title, 50)}</h2>
-									<Link to={`/detail/${vid.id}`}>View Detail</Link>
+									<Link to={`/detail/${vid.id}`}>
+										<span></span>View Detail
+									</Link>
 								</div>
 							</div>
 						</SwiperSlide>
 					);
 				})}
+
 				<Btns />
 			</Swiper>
 		</figure>
 	);
 }
+
+/*
+	React에서 Swiper의 코어기능을 적용하기 위해서는 useSwiper라는 hook호출
+	Swiper안쪽에서 또다른 컴포넌트를 연결해주고 그 안쪽에서 useSwiper로 부터 객체생성
+	해당 자식 자식 컴포넌트 안쪽에서 생성된 객체로부터 swiper core에 등록되어 있는 모든 메서드, 프로퍼티를 리액트에서도 사용가능
+*/
 
 /* 
 React에서는 swiper의 코어기능을 적용하기 위해서 useSwiper라는 hook 호출 

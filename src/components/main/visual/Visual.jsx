977ab4fda44aa0c46d51a464738539a1fbc8f329
phoebe7010 <phoebe7010@daum.net>
2024-01-03 15:14:30 +0900

    오른쪽 swiperBox 스타일링

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,6 +1,6 @@
-import './Visual.scss';
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
-import { Swiper, SwiperSlide } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './Visual.scss';
 import 'swiper/css';
 import { useRef, useState } from 'react';
 
@@ -13,20 +13,16 @@ export default function Visual() {
 		slidesPerView: 1,
 		spaceBetween: 0,
 		centeredSlides: true,
+		onSwiper: swiper => swiper.slideNext(300),
 		breakpoints: {
-			1000: {
-				sliderPerView: 2,
-				spaceBetween: 50
-			},
-			1400: {
-				sliderPerView: 3,
-				spaceBetween: 50
-			}
+			1000: { slidesPerView: 2 },
+			1400: { slidesPerView: 3 }
 		}
 	});
 
 	return (
 		<figure className='Visual'>
+			<div className='txtBox'></div>
 			<Swiper {...swiperOpt.current}>
 				{isSuccess &&
 					data.map((el, idx) => {

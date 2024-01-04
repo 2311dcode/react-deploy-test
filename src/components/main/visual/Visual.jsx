214bc21e6948b74869e5555ecb9c06e63b54b74a
phoebe7010 <phoebe7010@daum.net>
2024-01-04 10:01:34 +0900

    swiper 좌우 미리보기 버튼 생성 및 이벤트 연결

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,28 +1,44 @@
 import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
 import './Visual.scss';
 import 'swiper/css';
 import { useRef, useState, useEffect } from 'react';
 
 export default function Visual() {
 	const num = useRef(5);
+	const swipeRef = useRef(null);
 	const { isSuccess, data } = useYoutubeQuery();
-	const [Index, setIndex] = useState(0);
-	const [PrevIndex, setPrevIndex] = useState(4);
-	const [NextIndex, setNextIndex] = useState(1);
+
+	//loop값이 true시 초기 Index값을 0,1을 주면 안됨
+	//onSwipe 이벤트 발생시 자동적으로 realIndex값이 기존 Index값에 1을 뺀값으로 적용되므로
+	//useEffect에 의해서 prevIndex값이 0혹은 마지막 순번으로 변경되므로 기존 realIndex값과 중첩되서 버그발생
+	const [PrevIndex, setPrevIndex] = useState(1);
+	const [Index, setIndex] = useState(2);
+	const [NextIndex, setNextIndex] = useState(3);
 
 	const swiperOpt = useRef({
+		modules: [Autoplay],
 		loop: true,
 		slidesPerView: 1,
 		spaceBetween: 50,
 		centeredSlides: true,
-		onSwiper: swiper => swiper.slideNext(300),
+		onSwiper: swiper => (swipeRef.current = swiper),
 		onSlideChange: swiper => setIndex(swiper.realIndex),
+		//autoplay: { delay: 2000, disableOnInteraction: true },
 		breakpoints: {
 			1000: { slidesPerView: 2 },
 			1400: { slidesPerView: 3 }
 		}
 	});
+
+	const trimTitle = title => {
+		let resultTit = '';
+		if (title.includes('(')) resultTit = title.split('(')[0];
+		else if (title.includes('[')) resultTit = title.split('[')[0];
+		else resultTit = title;
+		return resultTit;
+	};
 
 	useEffect(() => {
 		Index === 0 ? setPrevIndex(num.current - 1) : setPrevIndex(Index - 1);
@@ -39,7 +55,7 @@ export default function Visual() {
 
 							return (
 								<li key={el.id} className={idx === Index ? 'on' : ''}>
-									<h3>{el.snippet.title}</h3>
+									<h3>{trimTitle(el.snippet.title)}</h3>
 								</li>
 							);
 						})}
@@ -49,7 +65,7 @@ export default function Visual() {
 			<Swiper {...swiperOpt.current}>
 				{isSuccess &&
 					data.map((el, idx) => {
-						if (idx >= num.current - 1) return null;
+						if (idx >= num.current) return null;
 						return (
 							<SwiperSlide key={el.id}>
 								<div className='pic'>
@@ -68,10 +84,10 @@ export default function Visual() {
 			<nav className='preview'>
 				{isSuccess && (
 					<>
-						<p className='prevBox'>
+						<p className='prevBox' onClick={() => swipeRef.current.slidePrev(400)}>
 							<img src={data[PrevIndex].snippet.thumbnails.default.url} alt={data[PrevIndex].snippet.title} />
 						</p>
-						<p className='nextBox'>
+						<p className='nextBox' onClick={() => swipeRef.current.slideNext(400)}>
 							<img src={data[NextIndex].snippet.thumbnails.default.url} alt={data[NextIndex].snippet.title} />
 						</p>
 					</>

986f6a7942167888107b404cb65a4148a41a1e42
phoebe7010 <phoebe7010@daum.net>
2024-01-04 12:15:39 +0900

    버튼 클릭시 해당 위치로 스크롤 모션 이동 Anime

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,5 +1,6 @@
 import './Btns.scss';
 import { useRef, useState, useEffect } from 'react';
+import Anime from '../../../asset/anime';
 
 export default function Btns() {
 	const [Index, setIndex] = useState(0);
@@ -17,31 +18,16 @@ export default function Btns() {
 				btns.current.children[idx].classList.add('on');
 			}
 		});
-		/*
-		if (scroll >= secs.current[0].offsetTop) {
-			Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
-			btns.current.children[0].classList.add('on');
-		}
-		if (scroll >= secs.current[1].offsetTop) {
-			Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
-			btns.current.children[1].classList.add('on');
-		}
-		if (scroll >= secs.current[2].offsetTop) {
-			Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
-			btns.current.children[2].classList.add('on');
-		}
-		if (scroll >= secs.current[3].offsetTop) {
-			Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
-			btns.current.children[3].classList.add('on');
-		}
-    */
 	};
 
 	useEffect(() => {
-		setNum(secs.current.length);
 		wrap.current = document.querySelector('.wrap');
 		secs.current = document.querySelectorAll('.myScroll');
+		setNum(secs.current.length);
+
 		wrap.current.addEventListener('scroll', activation);
+
+		return () => wrap.current.removeEventListener('scroll', activation);
 	}, []);
 
 	return (
@@ -49,7 +35,14 @@ export default function Btns() {
 			{Array(Num)
 				.fill()
 				.map((_, idx) => {
-					return <li key={idx} className={idx === Index ? 'on' : ''} onClick={() => setIndex(idx)}></li>;
+					return (
+						<li
+							key={idx}
+							className={idx === Index ? 'on' : ''}
+							onClick={() => {
+								new Anime(wrap.current, { scroll: secs.current[idx].offsetTop }, { duration: 500 });
+							}}></li>
+					);
 				})}
 		</ul>
 	);

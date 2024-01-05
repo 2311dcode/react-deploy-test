d91956cc39a167f26727d03b9a21a83955fbf15e
phoebe7010 <phoebe7010@daum.net>
2024-01-05 18:01:38 +0900

    scroll값이용해서 직접 style값 제어

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -3,35 +3,40 @@ import { useScroll } from '../../../hooks/useScroll';
 import './Pics.scss';
 
 export default function Pics() {
-	console.log('Pics');
 	const [Frame, setFrame] = useState(null);
 	const thisEl = useRef(null);
-	const boxEl = useRef(null);
+	const titEl = useRef(null);
+	const titEl2 = useRef(null);
 	const { getCurrentScroll } = useScroll(Frame);
 
-	//hook으로부터 현재 보정된 scroll값을 반환받아서
-	//해당 값을 참조객체의 style값으로 연동
-	//scroll값이 바뀔때마다 불필요하게 재랜더링하지 않으면서 스크롤값 스타일에 연동
 	const handleScroll = useCallback(() => {
-		const scroll = getCurrentScroll(thisEl.current);
-		scroll >= 0 && (boxEl.current.style.transform = `translateX(${scroll}px)`);
+		const scroll = getCurrentScroll(thisEl.current, -window.innerHeight / 2);
+
+		if (scroll >= 0) {
+			titEl.current.style.transform = `translateX(${scroll}px)`;
+			titEl.current.style.opacity = 1 - scroll / 800;
+			titEl2.current.style.transform = ` scale(${1 + scroll / 400}) translateX(${scroll}px)`;
+			titEl2.current.style.opacity = 1 - scroll / 500;
+		}
 	}, [getCurrentScroll]);
 
-	//컴포넌트 마운트시 wrap요소를 Frame State에담음
-	//State에 담겨있는 wrap요소는 최신 scrollTop을 가지고 있는 동적인 상태
 	useEffect(() => {
 		setFrame(thisEl.current?.closest('.wrap'));
 	}, []);
 
 	useEffect(() => {
-		//Frame에 scroll이벤트 연결
 		Frame?.addEventListener('scroll', handleScroll);
 		return () => Frame?.removeEventListener('scroll', handleScroll);
 	}, [Frame, handleScroll]);
 
 	return (
 		<section className='Pics myScroll' ref={thisEl}>
-			<div className='box' ref={boxEl}></div>
+			<h3 className='tit' ref={titEl}>
+				FLICKR
+			</h3>
+			<h4 className='tit2' ref={titEl2}>
+				PREIVIEW
+			</h4>
 		</section>
 	);
 }

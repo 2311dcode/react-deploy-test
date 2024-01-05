beee4ba3f7dd3d37e319e61ceac31cfbd184f24a
phoebe7010 <phoebe7010@daum.net>
2024-01-05 13:20:49 +0900

    children이 아닌 querySelectorAll로 처리해야 되는 이유

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -7,22 +7,23 @@ export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
 	const refTitle = useRef(null);
 	const splitText = useSplitText();
-	const { scrollTo } = useScroll('.wrap');
-	console.log(scrollTo);
+ 
 
 	useEffect(() => {
-		//scrollTo(0);
 		splitText(refTitle.current, title, 0.7, 0.1);
 		setTimeout(() => {
 			refFrame.current?.classList.add('on');
 		}, 300);
-	}, [splitText, title, scrollTo]);
+	}, [splitText, title]);
 
 	return (
 		<main ref={refFrame} className={`Layout ${title}`}>
 			<h1 ref={refTitle}>{title}</h1>
 			<div className='bar'></div>
 			{children}
+			<button className='btnTop' onClick={ }>
+				Top
+			</button>
 		</main>
 	);
 }

36d77325319967fed60433f8737d7c5c115147d8
phoebe7010 <phoebe7010@daum.net>
2024-01-05 14:41:59 +0900

    HTMLCollection, NodeList차이

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,7 +1,7 @@
 import Anime from '../asset/anime';
 import { useRef, useEffect } from 'react';
 
-export function useScroll(frame) {
+export function useScroll(frame = 'wrap') {
 	//선택자로 스크롤을 제어해야 되는 루트컴포넌트의 클래스명을 받아 DOM요소를 참조객체에 연결
 	const frameRef = useRef(null);
 
@@ -13,7 +13,7 @@ export function useScroll(frame) {
 	//컴포넌트 마운트시 frameRef에 선택자 담기
 	useEffect(() => {
 		frameRef.current = document.querySelector(frame);
-	}, []);
+	}, [frame]);
 
 	//scrollTo함수를 비구조화할당으로 뽑아내기 위해서 객체로 묶어서 반환
 	return { scrollTo };

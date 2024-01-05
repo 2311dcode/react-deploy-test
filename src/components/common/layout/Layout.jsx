3d031f656a549f28743134e844a9d72f1450fb66
phoebe7010 <phoebe7010@daum.net>
2024-01-05 12:02:25 +0900

    공통의 레이아웃 컴포넌트에서 btnTop버튼 추가

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,37 +1,31 @@
 import { useEffect, useRef } from 'react';
 import './Layout.scss';
 import { useSplitText } from '../../../hooks/useText';
+import { useScroll } from '../../../hooks/useScroll';
 
 export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
 	const refTitle = useRef(null);
 	const splitText = useSplitText();
+	const { scrollTo } = useScroll('.wrap');
+	console.log(scrollTo);
 
 	useEffect(() => {
+		//scrollTo(0);
 		splitText(refTitle.current, title, 0.7, 0.1);
 		setTimeout(() => {
 			refFrame.current?.classList.add('on');
 		}, 300);
-	}, [splitText, title]);
+	}, [splitText, title, scrollTo]);
 
 	return (
 		<main ref={refFrame} className={`Layout ${title}`}>
 			<h1 ref={refTitle}>{title}</h1>
 			<div className='bar'></div>
 			{children}
+			<button className='btnTop' onClick={() => scrollTo(0)}>
+				Top
+			</button>
 		</main>
 	);
 }
-
-/* 
-자식에 무거운 연산처리 함수 있음 - 패치 함수 한번만 받으면 되는 것 
-그러나 
-부모컴포넌트가 바뀌면 자식컴포넌트도 재렌더링 
-- useMemo 로 처리 
-props 가 없다면 가능 
-memo가 없다면 ?? 
-함수가 문제니까 함수를 useMemo 로 처리 
-
-hoc 고차 컴포넌트 
-
-*/

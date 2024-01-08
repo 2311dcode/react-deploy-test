8bb6531a1a938da0d9eb3c3493e0a5d2924fa401
phoebe7010 <phoebe7010@daum.net>
2024-01-08 10:16:13 +0900

    useScroll에서 frame state호출로 수정

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,19 +1,21 @@
 import Anime from '../asset/anime';
+import { useState, useEffect } from 'react';
 
 export function useScroll(scrollFrame) {
-	//기존에 scrollTop값을 제어하는 wrap요소를 참조객체에 담아서 반환하는것이 문제
-	//이유: wrap.scrollTop의 변경되는 값을 계속 활용해되는데
-	//참조객체에 담으면 담는 순간의 참조객체값이 고정되는 문제 발생
-	//해결방법: wrap요소를 호출 부모컴포넌트에서 State에 담도록 처리
+	const [Frame, setFrame] = useState(null);
 	const scrollTo = targetPos => {
-		new Anime(scrollFrame.current, { scroll: targetPos });
+		Frame && new Anime(Frame, { scroll: targetPos });
 	};
 
-	const getCurrentScroll = selfEl => {
-		const scroll = scrollFrame.scrollTop;
+	const getCurrentScroll = (selfEl, baseLine = 0) => {
+		const scroll = Frame.scrollTop - baseLine;
 		const modifiedScroll = scroll - selfEl?.offsetTop;
 		return modifiedScroll;
 	};
 
-	return { scrollTo, getCurrentScroll };
+	useEffect(() => {
+		setFrame(document.querySelector('.wrap'));
+	}, []);
+
+	return { scrollTo, getCurrentScroll, Frame };
 }

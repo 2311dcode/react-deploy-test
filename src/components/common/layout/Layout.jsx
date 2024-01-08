77a89efd11a0135a145e6d7fe7aceb40cc665a9b
phoebe7010 <phoebe7010@daum.net>
2024-01-08 11:50:49 +0900

    Layout컴포넌트 바뀐 useScroll구조에 따라 변경

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -1,36 +1,29 @@
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import './Layout.scss';
 import { useSplitText } from '../../../hooks/useText';
 import { useScroll } from '../../../hooks/useScroll';
 
 export default function Layout({ children, title }) {
-	const refFrame = useRef(null);
 	const refTitle = useRef(null);
 	const refBtnTop = useRef(null);
 	const splitText = useSplitText();
-	const { scrollTo, getCurrentScroll, scrollFrame } = useScroll();
 
-	const handleScroll = useCallback(
-		num => {
-			getCurrentScroll() >= num ? refBtnTop.current?.classList.add('on') : refBtnTop.current?.classList.remove('on');
-		},
-		[getCurrentScroll]
-	);
+	//순서1 - 레이아웃에서 공통적으로 적용될 스크롤이 100을 넘어가면 btnTop보이는 함수 정의
+	const handleCustomScroll = scroll => {
+		scroll >= 100 ? refBtnTop.current?.classList.add('on') : refBtnTop.current?.classList.remove('on');
+	};
+	const { scrollTo, refEl } = useScroll(handleCustomScroll, 0);
 
 	useEffect(() => {
 		scrollTo(0);
 		splitText(refTitle.current, title, 0.7, 0.1);
 		setTimeout(() => {
-			refFrame.current?.classList.add('on');
+			refEl.current?.classList.add('on');
 		}, 300);
-	}, [splitText, title, scrollTo]);
-
-	useEffect(() => {
-		scrollFrame.current.addEventListener('scroll', () => handleScroll(300));
-	}, [getCurrentScroll, handleScroll, scrollFrame]);
+	}, [splitText, title, scrollTo, refEl]);
 
 	return (
-		<main ref={refFrame} className={`Layout ${title}`}>
+		<main ref={refEl} className={`Layout ${title}`}>
 			<h1 ref={refTitle}>{title}</h1>
 			<div className='bar'></div>
 			{children}
